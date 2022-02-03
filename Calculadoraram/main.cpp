270278e072f7062c900d21d757ace42408690689
MeyglinRosales <mrosaleso3@miumg.edu.gt>
2022-02-03 01:35:29 -0600

    Cambio de la multiplicacion

--- Calculadoraram/main.cpp
+++ Calculadoraram/main.cpp
@@ -6,12 +6,12 @@ int main()
 {
     int Num1 = 0;
     int Num2 = 0;
-    cout << "Resta" << endl;
+    cout << "Multiplicacion" << endl;
     cout << "Introduce el primer numero: " << endl;
     cin >> Num1;
     cout << "Introduce e segundo numero" << endl;
     cin >> Num2;
-    cout << "La resta es: " << Num1 - Num2 << endl;
+    cout << "La Multiplicacion es: " << Num1 * Num2 << endl;
     return 0;
 
 }
